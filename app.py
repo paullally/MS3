007c22a63a39770ca57556d37a7c91170674b568
paullally <paullally98@gmail.com>
2021-09-01 14:04:17 +0000

    update template

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import os
-from flask import Flask
+import json
+import random
+import string
+import uuid
 from flask import Flask, render_template, session, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+import bcrypt
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def index():
-    return render_template('login.html')
+@app.route("/get_tasks")
+def test():
+    test = list(mongo.db.users.find())
+    print(test)
+    return render_template('login-layout.html')
+
 
 
 if __name__ == "__main__":
