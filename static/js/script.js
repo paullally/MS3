704d8152ca9ae21724ff448c7eaba7f7d7e536dd
paullally <paullally98@gmail.com>
2021-10-08 13:14:46 +0000

    add style to difficulty filter

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,51 @@
 $(window).on('load', function() {
     $('#staticBackdrop').appendTo("body").modal('show');
 })
+
+$("#filter5").hover(function(){
+    $("#filter5").css("color", "gold");
+    $("#filter4").css("color", "gold");
+    $("#filter3").css("color", "gold");
+    $("#filter2").css("color", "gold");
+    $("#filter1").css("color", "gold");
+    }, function(){
+        $("#filter5").css("color", "grey");
+        $("#filter4").css("color", "grey");
+        $("#filter3").css("color", "grey");
+        $("#filter2").css("color", "grey");
+        $("#filter1").css("color", "grey");
+  });
+
+  $("#filter4").hover(function(){
+    $("#filter4").css("color", "gold");
+    $("#filter3").css("color", "gold");
+    $("#filter2").css("color", "gold");
+    $("#filter1").css("color", "gold");
+    }, function(){
+       
+        $("#filter4").css("color", "grey");
+        $("#filter3").css("color", "grey");
+        $("#filter2").css("color", "grey");
+        $("#filter1").css("color", "grey");
+  });
+  $("#filter3").hover(function(){
+    $("#filter3").css("color", "gold");
+    $("#filter2").css("color", "gold");
+    $("#filter1").css("color", "gold");
+    }, function(){
+        $("#filter3").css("color", "grey");
+        $("#filter2").css("color", "grey");
+        $("#filter1").css("color", "grey");
+  });
+  $("#filter2").hover(function(){
+    $("#filter2").css("color", "gold");
+    $("#filter1").css("color", "gold");
+    }, function(){
+        $("#filter2").css("color", "grey");
+        $("#filter1").css("color", "grey");
+  });
+  $("#filter1").hover(function(){
+    $("#filter1").css("color", "gold");
+    }, function(){
+        $("#filter1").css("color", "grey");
+  });
